0308d2f673b520416cd5d96d9418309a3fa12b7d
Robow0 <robowsalad@gmail.com>
2023-09-05 11:39:08 +0300

    Piper updated

---
+++ Piper/meson_install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+if [ -z $DESTDIR ]; then
+	PREFIX=${MESON_INSTALL_PREFIX:-/usr}
+
+    # Update icon cache
+    gtk-update-icon-cache -f -t $PREFIX/share/icons/hicolor
+    
+    # Update desktop database cache
+    update-desktop-database
+
+    # Install new schemas
+    #glib-compile-schemas $PREFIX/share/glib-2.0/schemas/
+fi
